d2ce4e549e65618d38f2029f1b393a6241b52cde
syedzeik <01194267@pw.edu.pl>
2025-11-10 15:40:21 +0100

    Corrected writing rezults to file

--- z1.1/run.sh
+++ z1.1/run.sh
@@ -10,7 +10,7 @@ sleep 5
 
 cd client
 docker build -t z33_client_image .
-docker run -it --rm --name z33_client --network z33_network z33_client_image z33_server_image 8000
+docker run -it --rm --name z33_client --network z33_network -v "$(pwd):/app" z33_client_image
 cd ..
 
 docker stop z33_server
