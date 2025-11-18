e6c0b97101dbe19039537373571aadd8b1263c4e
Katsyaryna Anikevich <katsiaryna.anikevich.stud@pw.edu.pl>
2025-11-18 19:22:36 +0100

    small bug fixed

--- z1.1/run.sh
+++ z1.1/run.sh
@@ -10,7 +10,7 @@ sleep 5
 
 cd client
 docker build -t z33_client_image .
-docker run -it --rm --name z33_client --network z33_network -v "$(pwd):/app" z33_client_image
+docker run -it --rm --name z33_client --network z33_network -v "$(pwd):/app" -w /app z33_client_image z33_server_image
 cd ..
 
 docker stop z33_server
