913da50b104b45dac4308bf3692c573fbcc8a2d9
Kate <anick6916@gmail.com>
2026-01-11 18:12:11 +0100

    added base config

---
+++ project/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+cd server
+docker build -t z33_server_image .
+docker run -d --rm --name z33_server --network z33_network z33_server_image
+cd ..
+
+sleep 5
+
+cd client
+docker build -t z33_client_image .
+docker run -it --rm --name z33_client --network z33_network -v "$(pwd):/app" -w /app z33_client_image z33_server_image
+cd ..
+
+docker stop z33_server
+
